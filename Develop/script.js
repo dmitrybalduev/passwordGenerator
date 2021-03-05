06d7dda2e83943b18f5eb8afe2a84222cdf45e2e
Dmitry <dmitry.balduev@gmail.com>
2021-03-05 17:23:09 -0500

    added function to ask other criterias for password

--- Develop/script.js
+++ Develop/script.js
@@ -25,6 +25,7 @@ let hasLowerCase = false;
 let hasNumeric = false;
 let hasSpecials = false;
 
+//function to ask user desired length of password
 function askLengthPassword(){
   //creating count to use it for messaging in case if user enters not valid length
   let count = 0; 
@@ -43,6 +44,35 @@ function askLengthPassword(){
     passwordLength = parseInt(input);
   return passwordLength;
 }
+
+//function to ask user if user wants to include lower case letters
+function askLowerCase(){
+  let input = confirm("Would you like to include lower case letters?");
+  hasLowerCase = input;
+  return input;
+}
+
+//function to ask user if user wants to include upper case letters
+function askUpperCase(){
+  let input = confirm("Would you like to include upper case letters?");
+  hasUpperCase = input;
+  return input;
+}
+
+//function to ask user if user wants to include numeric values
+function askNumeric(){
+  let input = confirm("Would you like to include numeric values?");
+  hasNumeric = input;
+  return input;
+}
+
+//function to ask user if user wants to include special characters
+function askSpecials(){
+  let input = confirm("Would you like to include special characters");
+  hasSpecials = input;
+  return input;
+}
+
 
 function generatePassword(){
   console.log(askLengthPassword()); 
