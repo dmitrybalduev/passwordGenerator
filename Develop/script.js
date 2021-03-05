94eeb949e8a9c2d1046da2bfffdce9dc6d0e0bdc
Dmitry <dmitry.balduev@gmail.com>
2021-03-05 17:13:03 -0500

    Added function to ask user for password length

--- Develop/script.js
+++ Develop/script.js
@@ -19,16 +19,31 @@ let numbers = "1234567890".split("");
 let specials = "`~!@#$%^&*()-_+={}[]:;'?/.>,<|\\\"".split("");
 
 console.log(specials);
-let passwordLength = 0;
+let passwordLength = "1a";
 let hasUpperCase = false;
 let hasLowerCase = false;
 let hasNumeric = false;
 let hasSpecials = false;
 
 function askLengthPassword(){
-  passwordLength = prompt("Please choose length of password.\nMinimum is 8, maximum is 128");
+  //creating count to use it for messaging in case if user enters not valid length
+  let count = 0; 
+  let input;
+  //while loop to keep asking user to enter a valid number
+  //check if value is numeric and fall into boundaries
+   while(isNaN(passwordLength) || (passwordLength < 8 || passwordLength > 128)){
+     if(count === 0){
+      input = prompt("Please choose length of password.\nMinimum is 8, maximum is 128");
+     } else{
+       //if count is not 0, ask user to enter valid numeric value
+      input = prompt("Please choose valid length of password (Make sure you enter numeric value).\nMinimum is 8, maximum is 128");
+     }
+     count++;
+    }
+    passwordLength = parseInt(input);
+  return passwordLength;
 }
 
 function generatePassword(){
-  
+  console.log(askLengthPassword()); 
 }
